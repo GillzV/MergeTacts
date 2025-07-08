58d05e523548c764a1d65e9e76191afecd2bf3a5
GillzV <Gillv1027@gmail.com>
2025-07-07 21:45:16 -0700

    removed unnecessary

--- run_as_admin.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-echo Running Card Placer as Administrator...
-powershell -Command "Start-Process cmd -ArgumentList '/k cd /d \"%~dp0\" && python auto_card_placer.py' -Verb RunAs"
-pause 
--- run_no_admin.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo Running Card Placer (No Admin Version)...
-echo.
-echo This version uses alternative methods to avoid requiring administrator privileges.
-echo If mouse control doesn't work well, the script will suggest keyboard shortcuts.
-echo.
-python auto_card_placer_no_admin.py
-pause 
